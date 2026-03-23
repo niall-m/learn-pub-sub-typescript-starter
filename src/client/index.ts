d05909a9f912fab4016ddf4bf1f5b7143d55b277
Niall Mahford <niall.mahford@gmail.com>
2026-03-23 16:53:45 -0400

    consume pause on client

--- src/client/index.ts
+++ src/client/index.ts
@@ -6,11 +6,16 @@ import {
   printClientHelp,
   printQuit,
 } from "../internal/gamelogic/gamelogic.js";
-import { declareAndBind, SimpleQueueType } from "../internal/pubsub/consume.js";
+import {
+  declareAndBind,
+  SimpleQueueType,
+  subscribeJSON,
+} from "../internal/pubsub/consume.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
 import { GameState } from "../internal/gamelogic/gamestate.js";
 import { commandSpawn } from "../internal/gamelogic/spawn.js";
 import { commandMove } from "../internal/gamelogic/move.js";
+import { handlerPause } from "./handlers.js";
 
 async function main() {
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
@@ -31,16 +36,16 @@ async function main() {
   );
 
   const username = await clientWelcome();
+  const gs = new GameState(username);
 
-  await declareAndBind(
+  await subscribeJSON(
     conn,
     ExchangePerilDirect,
     `${PauseKey}.${username}`,
     PauseKey,
     SimpleQueueType.Transient,
+    handlerPause(gs),
   );
-
-  const gs = new GameState(username);
 
   while (true) {
     const words = await getInput();
