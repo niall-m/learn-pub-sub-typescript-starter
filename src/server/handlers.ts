6edbcbdbde2e64534d1a3d89dd401c3760402958
Niall Mahford <niall.mahford@gmail.com>
2026-03-24 13:51:31 -0400

    fix await bug

--- src/server/handlers.ts
+++ src/server/handlers.ts
@@ -4,7 +4,7 @@ import { AckType } from "../internal/pubsub/consume.js";
 export function handlerLog() {
   return async (gamelog: GameLog): Promise<AckType> => {
     try {
-      writeLog(gamelog);
+      await writeLog(gamelog);
       return AckType.Ack;
     } catch (err) {
       console.error("Error writing log:", err);
